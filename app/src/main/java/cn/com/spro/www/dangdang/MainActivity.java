76753bd08e82a43d930bffa8c42eaad6c556c2a3
Horton <firetim@126.com>
2015-09-16 14:58:59 +0800

    v1.0.1 Toolbar+DrawerLayout+FAB+RecyclerView+TabBar+CardView

--- app/src/main/java/cn/com/spro/www/dangdang/MainActivity.java
+++ app/src/main/java/cn/com/spro/www/dangdang/MainActivity.java
@@ -135,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
